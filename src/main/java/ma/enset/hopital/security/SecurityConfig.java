9adfd930326eac2f760d75547733c8e3996c33f3
nouhaila <nouhaila@exemple.com>
2025-06-07 21:36:58 +0000

    Ajout de la configuration de la securite

--- src/main/java/ma/enset/hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/hopital/security/SecurityConfig.java
@@ -1,4 +1,58 @@
 package ma.enset.hopital.security;
 
+import lombok.AllArgsConstructor;
+import ma.enset.hopital.service.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
+@AllArgsConstructor
 public class SecurityConfig {
+    private PasswordEncoder passwordEncoder;
+    private UserDetailsServiceImpl userDetailsServiceImpl;
+
+    //@Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    //@Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("1234")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.formLogin().loginPage("/login").defaultSuccessUrl("/", true).permitAll();
+        httpSecurity.rememberMe();
+        httpSecurity.authorizeHttpRequests().requestMatchers("/webjars/**", "/h2-console/**").permitAll();
+        // httpSecurity.authorizeHttpRequests().requestMatchers("/user/**").hasRole("USER");
+        // httpSecurity.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN");
+        httpSecurity.authorizeHttpRequests().anyRequest().authenticated();
+        httpSecurity.exceptionHandling().accessDeniedPage("/notAuthorized");
+        httpSecurity.userDetailsService(userDetailsServiceImpl);
+        httpSecurity.logout()
+                .logoutUrl("/logout") // URL de déconnexion
+                .logoutSuccessUrl("/login?logout") // Redirection après déconnexion
+                .invalidateHttpSession(true) // Invalider la session
+                .deleteCookies("JSESSIONID", "remember-me"); // Supprimer les cookies
+        return httpSecurity.build();
+    }
 }
