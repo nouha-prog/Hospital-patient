2bca74cd3a6e43581574a7c46598d2bfdeb6ccfe
nouhaila <nouhaila@exemple.com>
2025-06-07 23:13:45 +0000

    Amelioration d´HopitalApplication

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -55,4 +55,52 @@ public class HopitalApplication {
 			}
 		};
 	}
+
+
+	//@Bean
+	CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager) {
+		PasswordEncoder passwordEncoder = passwordEncoder();
+		return args -> {
+			UserDetails u1 = jdbcUserDetailsManager.loadUserByUsername("user11");
+			if (u1 == null) {
+				jdbcUserDetailsManager.createUser(
+						User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+				);
+			}
+			UserDetails u2 = jdbcUserDetailsManager.loadUserByUsername("user22");
+			if (u2 == null) {
+				jdbcUserDetailsManager.createUser(
+						User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+				);
+			}
+			UserDetails u3 = jdbcUserDetailsManager.loadUserByUsername("admin2");
+			if (u3 == null) {
+				jdbcUserDetailsManager.createUser(
+						User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+				);
+			}
+		};
+	}
+
+	//@Bean
+	CommandLineRunner commandLineRunnerUserDetails(AccountService accountService) {
+		return args -> {
+			accountService.addNewRole("USER");
+			accountService.addNewRole("ADMIN");
+
+			accountService.addNewUser("user1", "1234", "user1@gmail.com", "1234");
+			accountService.addNewUser("user2", "1234", "user2@gmail.com", "1234");
+			accountService.addNewUser("admin", "1234", "admin@gmail.com", "1234");
+
+			accountService.addRoleToUser("user1", "USER");
+			accountService.addRoleToUser("user2", "USER");
+			accountService.addRoleToUser("admin", "USER");
+			accountService.addRoleToUser("admin", "ADMIN");
+		};
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
+	}
 }
